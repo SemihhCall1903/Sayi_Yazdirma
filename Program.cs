e6b75fce1c22f1d2580b1fdd2040df96a735ae51
Semih Arda Çallı <77378627+SemihhCall1903@users.noreply.github.com>
2021-10-29 16:42:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Sayi_Yazdirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baslangic, bitis;
+
+            Console.Write("Başlangıç Değerini Giriniz : ");
+            baslangic=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bitiş Değerini Giriniz : ");
+            bitis=Convert.ToInt32(Console.ReadLine());
+
+            while (baslangic <= bitis)
+            {
+                if (baslangic % 2 == 0 && baslangic % 3 == 0)
+                {
+                    Console.Write("\n" + baslangic.ToString());
+                    Console.ReadLine();
+                }
+
+                baslangic++;
+               
+            }
+        }
+    }
+}
